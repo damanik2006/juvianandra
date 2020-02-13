d290ed5a62c69b8d1b6c4c31e7f447b08c833a6e
Henni Hendrani <hhendranyn@gmail.com>
2020-02-13 09:32:13 +0700

    menampilkan data

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "",
-    USER: "",
-    PASSWORD: "",
-    DATABASE: ""
+    HOST: "us-cdbr-iron-east-04.cleardb.net",
+    USER: "bc12ada385e80d",
+    PASSWORD: "0619fbbb",
+    DATABASE: "heroku_1806982d8f5620b"
 };
